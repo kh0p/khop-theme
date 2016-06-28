1c0ebac2a3026305dbe5797f06dd965f104aa4c6
楊文里 <kh0p@users.noreply.github.com>
2016-06-28 11:54:36 +0200

    Creating basic site scheme

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('default', function () {
   });
   gulp.watch(['./source/css/*.scss','./source/css/**/_*.scss'],['sass']);
   gulp.watch(['./source/css/*.sass','./source/css/**/_*.sass'],['sass']);
-  gulp.watch(['./source/jade/*.jade', './source/jade/**/_*.jade'],['jade']);
+  gulp.watch(['./source/jade/*.jade', './source/jade/**/*.jade', './source/jade/**/_*.jade'],['jade']);
   gulp.watch(['./source/js/*.js'],['compile-js']);
 });
 
