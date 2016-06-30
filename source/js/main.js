b4f81defe74382f9c50cc6c9fa605d453fc04e49
楊文里 <kh0p@users.noreply.github.com>
2016-06-30 20:17:50 +0200

    Navbar

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   window.onload = function () {
     $('button').click(function() {
-      $(this).toggleClass("is-active");
+      $(this).toggleClass('is-active');
+      $('#navigation').animate({visibility: visible}, 300).fadeIn(500);
     });
     var mySVG = $('#dragon-svg').drawsvg();
     mySVG.drawsvg('animate');
